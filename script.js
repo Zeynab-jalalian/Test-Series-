3905308833d7a16e35ad403610fbca683ad10b23
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-11-26 13:30:14 +0330

    first button

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const start_container = document.querySelector(".start-container");
 const startGame = document.querySelector(".startGame");
 const category = document.querySelector(".category");
+const Dexter=document.querySelector(".Dexter");
+const questionBox=document.querySelector(".question-box");
 
 startGame.addEventListener("click", () => {
   start_container.classList.add("hidden");
   category.classList.remove("hidden");
+});
+Dexter.addEventListener("click", () => {
+  category.classList.add("hidden");
+  questionBox.classList.remove("hidden");
 });
